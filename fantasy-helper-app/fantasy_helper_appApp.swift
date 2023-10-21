c6f6fedb18ab86a0da8c7027eb2dccf775b536d9
jordengerovac <jordengerovac@gmail.com>
2023-10-20 21:23:19 -0400

    Initial Commit

---
+++ fantasy-helper-app/fantasy_helper_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fantasy_helper_appApp.swift
+//  fantasy-helper-app
+//
+//  Created by Jorden Gerovac on 2023-10-20.
+//
+
+import SwiftUI
+
+@main
+struct fantasy_helper_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
